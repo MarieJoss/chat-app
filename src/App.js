5ded7b6c964b4aa40630144cd30adf8fe904c63c
Marie Joss <mjosselin6@gmail.com>
2020-04-01 11:44:00 +0200

    add props

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <Contact/>
+     <Contact name="JC" avatar="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185" text="Online" online />
+     <Contact name="Bart Simpson" avatar="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FBartSimpson.png?1497567511638" text="Offline" offline/>
+     <Contact name="Nelson Muntz" avatar="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185" text="Online" online/>
     </div>
   );
 }
